a703e7a0ba04b2765ed2f1f11861d1d1b8b09a93
Ruizitoxd <juanca17112005@gmail.com>
2025-01-26 03:33:40 -0500

    Segundo juego Completo

--- Assets/Scripts/Ruiz/ProtectBubble/ExplodeBubble.cs
+++ Assets/Scripts/Ruiz/ProtectBubble/ExplodeBubble.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ExplodeBubble : MonoBehaviour
@@ -14,5 +15,19 @@ public class ExplodeBubble : MonoBehaviour
     {
         //Destroy(gameObject);
         BubbleController.instance.pool.ReturnBubble(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Explode();
+        Transform parentTransform = collision.gameObject.transform;
+        foreach (Transform child in parentTransform)
+        {
+            child.gameObject.SetActive(true);
+        }
+        collision.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        collision.GetComponent<Rigidbody2D>().gravityScale = -0.9f;
+        collision.GetComponent<ParticleSystem>().Play();
+        collision.GetComponent<Collider2D>().enabled = false;
     }
 }
